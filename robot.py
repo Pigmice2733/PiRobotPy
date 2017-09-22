0d338ee82da40d1a46a1f3c88ebb8c37a42b457b
Pat Hickey <pat@moreproductive.org>
2017-09-21 18:19:08 -0700

    add a vanilla robot.py

---
+++ robot.py
@@ -0,0 +1,42 @@
+"""
+    This is the generic robotpy skeleton. We are going to work to make this run
+    on platforms besides the RoboRIO.
+"""
+
+import wpilib
+
+class Minibot(wpilib.IterativeRobot):
+
+    def robotInit(self):
+        """
+        This function is called upon program startup and
+        should be used for any initialization code.
+        """
+        self.robot_drive = wpilib.RobotDrive(0,1)
+        self.stick = wpilib.Joystick(1)
+
+    def autonomousInit(self):
+        """This function is run once each time the robot enters autonomous mode."""
+        self.auto_loop_counter = 0
+
+    def autonomousPeriodic(self):
+        """This function is called periodically during autonomous."""
+
+        # Check if we've completed 100 loops (approximately 2 seconds)
+        if self.auto_loop_counter < 100:
+            self.robot_drive.drive(-0.5, 0) # Drive forwards at half speed
+            self.auto_loop_counter += 1
+        else:
+            self.robot_drive.drive(0, 0)    #Stop robot
+
+    def teleopPeriodic(self):
+        """This function is called periodically during operator control."""
+        self.robot_drive.arcadeDrive(self.stick)
+
+    def testPeriodic(self):
+        """This function is called periodically during test mode."""
+        wpilib.LiveWindow.run()
+
+if __name__ == "__main__":
+    wpilib.run(Minibot)
+

ba1eeab42319b9eaee09efe3c12b521d328fea96
Benjamin Green <benjaminisaacgreen@gmail.com>
2017-09-26 15:49:06 -0700

    added MotorControl to init and imports

--- pwmoutput.py
+++ pwmoutput.py
@@ -3,6 +3,7 @@ import time
 import hal
 from threading import Thread
 from hal_impl.data import hal_data
+from micro_maestro_interface import MotorControl
 
 
 def talon_outputs():
@@ -17,18 +18,22 @@ def talon_outputs():
 class PwmWatch(Thread):
     def __init__(self, output=None):
         Thread.__init__(self)
+        self.MotorControl = MotorControl
         self.output = output
         self.daemon = True
         self.start()
 
     def run(self):
+        MotorControl.set_bounds(0.6, 0.08, 0.0, 0.19, 0.6)
         while True:
             time.sleep(0.05)
             outputs = talon_outputs()
             if self.output:
                 # TODO: hook to micro maestro here
+                
                 pass
             else:
                 for k, v in outputs.items():
                     if v != None:
                         print("chan {} output {}".format(k, v))
+            
